d4f892f2abdf2ae75267da777d514c1181e8bd13
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2024-11-28 16:23:39 +0100

    Trying to add players to the stadium from the players slider

--- assets/script.js
+++ assets/script.js
@@ -36,21 +36,18 @@ fetch('https://theshamkhi.github.io/FUTXpert/assets/data/data.json')
                     <!-- Player Card Top -->
                     <div class="flex relative text-[#e9cc74]">
                         <div class="absolute text-left pt-[2rem] text-uppercase">
-                            <div class="text-2xl font-light">${player.rating}</div> <!-- Rating -->
-                            <div class="text-xl font-light">${player.position}</div> <!-- Position -->
+                            <div class="text-2xl font-light">${player.rating}</div>
+                            <div class="text-xl font-light">${player.position}</div>
                             
-                            <!-- Flag -->
                             <div class="block w-[2rem] h-[25px] my-[0.3rem]">
                                 <img class="w-full h-full object-contain" src="${player.flag}" alt="${player.nationality}">
                             </div>
                             
-                            <!-- Club Logo -->
                             <div class="block w-[2.1rem] h-[40px]">
                                 <img class="w-full h-full object-contain" src="${player.logo}" alt="${player.club}">
                             </div>
                         </div>
                         
-                        <!-- Player Picture -->
                         <div class="w-[220px] h-auto mx-auto overflow-hidden">
                             <img class="w-full h-full object-contain relative bottom-0" src="${player.photo}" alt="${player.name}">
                         </div>
@@ -60,12 +57,10 @@ fetch('https://theshamkhi.github.io/FUTXpert/assets/data/data.json')
                     <div class="relative">
                         <div class="text-[#e9cc74] w-[90%] mx-auto py-[0.3rem_0]">
                             
-                            <!-- Player Name -->
                             <div class="text-center text-xl uppercase border-b-2 border-[#e9cc74] py-[1rem]">
                                 <span class="text-shadow">${player.name}</span>
                             </div>
                 
-                            <!-- Player Features -->
                             <div class="flex justify-center my-[0.5rem]">`;
 
             // Condition for goalkeepers
@@ -130,3 +125,96 @@ fetch('https://theshamkhi.github.io/FUTXpert/assets/data/data.json')
         });
     })
     .catch(error => console.error('Error fetching player data:', error));
+
+
+// Handle form submission
+const playerForm = document.getElementById('playerForm');
+
+playerForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    // Extract form values
+    const name = document.getElementById('name').value;
+    const photo = document.getElementById('photo').value;
+    const position = document.getElementById('position').value;
+    const nationality = document.getElementById('nationality').value;
+    const flag = document.getElementById('flag').value;
+    const club = document.getElementById('club').value;
+    const logo = document.getElementById('logo').value;
+    const rating = document.getElementById('rating').value;
+    const pace = document.getElementById('pace').value;
+    const shooting = document.getElementById('shooting').value;
+    const passing = document.getElementById('passing').value;
+    const dribbling = document.getElementById('dribbling').value;
+    const defending = document.getElementById('defending').value;
+    const physical = document.getElementById('physical').value;
+    const chosen = document.getElementById('LStriker');
+
+    chosen.innerHTML = `
+        <div class="absolute w-[12vw] h-auto bg-cover top-[25%] left-[19%] bg-center p-[2.5rem] z-10" 
+            style="background-image: url('https://selimdoyranli.com/cdn/fut-player-card/img/card_bg.png');">
+            <!-- Top Section -->
+            <div class="flex flex-col items-center text-[#e9cc74] relative">
+                <!-- Rating and Position -->
+                <div class="absolute top-0 left-0 text-left">
+                    <div class="text-2xl font-light">${rating}</div>
+                    <div class="text-xl font-light">${position}</div>
+                    <div class="w-[2rem] h-[25px] my-[0.3rem]">
+                        <img class="w-full h-full object-contain" src="${flag}" alt="${nationality}">
+                    </div>
+                    <div class="w-[2.1rem] h-[40px]">
+                        <img class="w-full h-full object-contain" src="${logo}" alt="${club}">
+                    </div>
+                </div>
+                <!-- Player Photo -->
+                <div class="relative w-[80%] h-auto mx-auto overflow-hidden z-10">
+                    <img class="w-full h-full object-contain" src="${photo}" alt="${name}">
+                </div>
+            </div>
+            <!-- Name Section -->
+            <div class="text-[#e9cc74] w-[90%] mx-auto mt-[1rem]">
+                <div class="text-center text-xl uppercase border-b-2 border-[#e9cc74] py-[1rem]">
+                    <span class="text-shadow">${name}</span>
+                </div>
+            </div>
+            <!-- Stats Section -->
+            <div class="flex justify-between w-[90%] mx-auto mt-[0.5rem] text-[#e9cc74]">
+                <!-- Left Stats -->
+                <div class="flex flex-col items-end pr-[1rem] border-r-2 border-[#e9cc74]">
+                    <span class="flex text-lg uppercase">
+                        <span class="font-bold mr-[0.3rem]">${pace}</span><span class="font-light">PAC</span>
+                    </span>
+                    <span class="flex text-lg uppercase">
+                        <span class="font-bold mr-[0.3rem]">${shooting}</span><span class="font-light">SHO</span>
+                    </span>
+                    <span class="flex text-lg uppercase">
+                        <span class="font-bold mr-[0.3rem]">${passing}</span><span class="font-light">PAS</span>
+                    </span>
+                </div>
+                <!-- Right Stats -->
+                <div class="flex flex-col items-start pl-[1rem]">
+                    <span class="flex text-lg uppercase">
+                        <span class="font-bold mr-[0.3rem]">${dribbling}</span><span class="font-light">DRI</span>
+                    </span>
+                    <span class="flex text-lg uppercase">
+                        <span class="font-bold mr-[0.3rem]">${defending}</span><span class="font-light">DEF</span>
+                    </span>
+                    <span class="flex text-lg uppercase">
+                        <span class="font-bold mr-[0.3rem]">${physical}</span><span class="font-light">PHY</span>
+                    </span>
+                </div>
+            </div>
+        </div>
+
+    `;
+
+    // Append card to the stadium
+    // const stadium = document.querySelector('.players');
+    // const addButton = document.getElementById('LStriker');
+
+    //stadium.insertBefore(document.createRange().createContextualFragment(cardHTML), addButton);
+
+    CloseModal();
+
+    playerForm.reset();
+});
