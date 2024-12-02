e1be90d3be9df15d39b0a235a5b8709981a899f9
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2024-12-02 01:09:59 +0100

    Added the remaining players in a slider

--- assets/script.js
+++ assets/script.js
@@ -61,10 +61,8 @@ document.getElementById('playerForm').addEventListener('submit', function (e) {
 function populatePlayerCard(player) {
     const { position, name, photo, nationality, flag, club, logo, rating, diving, handling, kicking, reflexes, speed, positioning, pace, shooting, passing, dribbling, defending, physical } = player;
 
-    console.log('Player position:', position); // Add this to debug
-
     if (!position) {
-        console.error('Invalid position:', position); // This will log if position is empty
+        console.error('Invalid position:', position);
         return;
     }
 
@@ -327,105 +325,115 @@ function modifyPlayer(position) {
 
 
 // // Fetch data from the JSON file
-// fetch('https://theshamkhi.github.io/FUTXpert/assets/data/data.json')
-//     .then(response => response.json())
-//     .then(data => {
-//         const players = data.players; // Get players array
-//         const playerSlider = document.getElementById('playerSlider');
-        
-//         players.forEach(player => {
-//             let cardHTML = `
-//                 <div class="relative w-[300px] h-[485px] bg-cover bg-center p-[2.5rem] z-10" style="background-image: url('https://selimdoyranli.com/cdn/fut-player-card/img/card_bg.png');">
+const savedPlayers = JSON.parse(localStorage.getItem('players')) || []; // players on localStorage
+
+// Fetch player data
+fetch('https://theshamkhi.github.io/FUTXpert/assets/data/data.json')
+    .then(response => response.json())
+    .then(data => {
+        const players = data.players; // Get players array
+        const playerSlider = document.getElementById('playerSlider');
+
+        // Filter players that are not in localStorage based on player name
+        const playersNotInLocalStorage = players.filter(player => {
+            return !savedPlayers.some(savedPlayer => savedPlayer.name === player.name);
+        });
+
+        // Loop through and create player cards for players not in localStorage
+        playersNotInLocalStorage.forEach(player => {
+            let cardHTML = `
+                <div class="relative w-[300px] h-[485px] bg-cover bg-center p-[2.5rem] z-10" style="background-image: url('https://selimdoyranli.com/cdn/fut-player-card/img/card_bg.png');">
                     
-//                     <!-- Player Card Top -->
-//                     <div class="flex relative text-[#e9cc74]">
-//                         <div class="absolute text-left pt-[2rem] text-uppercase">
-//                             <div class="text-2xl font-light">${player.rating}</div>
-//                             <div class="text-xl font-light">${player.position}</div>
+                    <!-- Player Card Top -->
+                    <div class="flex relative text-[#e9cc74]">
+                        <div class="absolute text-left pt-[2rem] text-uppercase">
+                            <div class="text-2xl font-light">${player.rating}</div>
+                            <div class="text-xl font-light">${player.position}</div>
                             
-//                             <div class="block w-[2rem] h-[25px] my-[0.3rem]">
-//                                 <img class="w-full h-full object-contain" src="${player.flag}" alt="${player.nationality}">
-//                             </div>
+                            <div class="block w-[2rem] h-[25px] my-[0.3rem]">
+                                <img class="w-full h-full object-contain" src="${player.flag}" alt="${player.nationality}">
+                            </div>
                             
-//                             <div class="block w-[2.1rem] h-[40px]">
-//                                 <img class="w-full h-full object-contain" src="${player.logo}" alt="${player.club}">
-//                             </div>
-//                         </div>
+                            <div class="block w-[2.1rem] h-[40px]">
+                                <img class="w-full h-full object-contain" src="${player.logo}" alt="${player.club}">
+                            </div>
+                        </div>
                         
-//                         <div class="w-[220px] h-auto mx-auto overflow-hidden">
-//                             <img class="w-full h-full object-contain relative bottom-0" src="${player.photo}" alt="${player.name}">
-//                         </div>
-//                     </div>
+                        <div class="w-[220px] h-auto mx-auto overflow-hidden">
+                            <img class="w-full h-full object-contain relative bottom-0" src="${player.photo}" alt="${player.name}">
+                        </div>
+                    </div>
                 
-//                     <!-- Player Card Bottom -->
-//                     <div class="relative">
-//                         <div class="text-[#e9cc74] w-[90%] mx-auto py-[0.3rem_0]">
+                    <!-- Player Card Bottom -->
+                    <div class="relative">
+                        <div class="text-[#e9cc74] w-[90%] mx-auto py-[0.3rem_0]">
                             
-//                             <div class="text-center text-xl uppercase border-b-2 border-[#e9cc74] py-[1rem]">
-//                                 <span class="text-shadow">${player.name}</span>
-//                             </div>
+                            <div class="text-center text-xl uppercase border-b-2 border-[#e9cc74] py-[1rem]">
+                                <span class="text-shadow">${player.name}</span>
+                            </div>
                 
-//                             <div class="flex justify-center my-[0.5rem]">`;
+                            <div class="flex justify-center my-[0.5rem]">`;
 
-//             // Condition for goalkeepers
-//             if (player.position === "GK") {
-//                 cardHTML += `
-//                     <div class="border-r-2 pr-[2.3rem]">
-//                         <span class="flex text-lg uppercase">
-//                             <span class="font-bold mr-[0.3rem]">${player.diving}</span><span class="font-light">DIV</span>
-//                         </span>
-//                         <span class="flex text-lg uppercase">
-//                             <span class="font-bold mr-[0.3rem]">${player.handling}</span><span class="font-light">HAN</span>
-//                         </span>
-//                         <span class="flex text-lg uppercase">
-//                             <span class="font-bold mr-[0.3rem]">${player.kicking}</span><span class="font-light">KIC</span>
-//                         </span>
-//                     </div>
-//                     <div class="pl-[2.3rem]">
-//                         <span class="flex text-lg uppercase">
-//                             <span class="font-bold mr-[0.3rem]">${player.reflexes}</span><span class="font-light">REF</span>
-//                         </span>
-//                         <span class="flex text-lg uppercase">
-//                             <span class="font-bold mr-[0.3rem]">${player.speed}</span><span class="font-light">SPD</span>
-//                         </span>
-//                         <span class="flex text-lg uppercase">
-//                             <span class="font-bold mr-[0.3rem]">${player.positioning}</span><span class="font-light">POS</span>
-//                         </span>
-//                     </div>`;
-//             } else {
-//                 cardHTML += `
-//                     <div class="border-r-2 pr-[2.3rem]">
-//                         <span class="flex text-lg uppercase">
-//                             <span class="font-bold mr-[0.3rem]">${player.pace}</span><span class="font-light">PAC</span>
-//                         </span>
-//                         <span class="flex text-lg uppercase">
-//                             <span class="font-bold mr-[0.3rem]">${player.shooting}</span><span class="font-light">SHO</span>
-//                         </span>
-//                         <span class="flex text-lg uppercase">
-//                             <span class="font-bold mr-[0.3rem]">${player.passing}</span><span class="font-light">PAS</span>
-//                         </span>
-//                     </div>
-//                     <div class="pl-[2.3rem]">
-//                         <span class="flex text-lg uppercase">
-//                             <span class="font-bold mr-[0.3rem]">${player.dribbling}</span><span class="font-light">DRI</span>
-//                         </span>
-//                         <span class="flex text-lg uppercase">
-//                             <span class="font-bold mr-[0.3rem]">${player.defending}</span><span class="font-light">DEF</span>
-//                         </span>
-//                         <span class="flex text-lg uppercase">
-//                             <span class="font-bold mr-[0.3rem]">${player.physical}</span><span class="font-light">PHY</span>
-//                         </span>
-//                     </div>`;
-//             }
+            // Condition for goalkeepers
+            if (player.position === "GK") {
+                cardHTML += ` 
+                    <div class="border-r-2 pr-[2.3rem]">
+                        <span class="flex text-lg uppercase">
+                            <span class="font-bold mr-[0.3rem]">${player.diving}</span><span class="font-light">DIV</span>
+                        </span>
+                        <span class="flex text-lg uppercase">
+                            <span class="font-bold mr-[0.3rem]">${player.handling}</span><span class="font-light">HAN</span>
+                        </span>
+                        <span class="flex text-lg uppercase">
+                            <span class="font-bold mr-[0.3rem]">${player.kicking}</span><span class="font-light">KIC</span>
+                        </span>
+                    </div>
+                    <div class="pl-[2.3rem]">
+                        <span class="flex text-lg uppercase">
+                            <span class="font-bold mr-[0.3rem]">${player.reflexes}</span><span class="font-light">REF</span>
+                        </span>
+                        <span class="flex text-lg uppercase">
+                            <span class="font-bold mr-[0.3rem]">${player.speed}</span><span class="font-light">SPD</span>
+                        </span>
+                        <span class="flex text-lg uppercase">
+                            <span class="font-bold mr-[0.3rem]">${player.positioning}</span><span class="font-light">POS</span>
+                        </span>
+                    </div>`;
+            } else {
+                cardHTML += ` 
+                    <div class="border-r-2 pr-[2.3rem]">
+                        <span class="flex text-lg uppercase">
+                            <span class="font-bold mr-[0.3rem]">${player.pace}</span><span class="font-light">PAC</span>
+                        </span>
+                        <span class="flex text-lg uppercase">
+                            <span class="font-bold mr-[0.3rem]">${player.shooting}</span><span class="font-light">SHO</span>
+                        </span>
+                        <span class="flex text-lg uppercase">
+                            <span class="font-bold mr-[0.3rem]">${player.passing}</span><span class="font-light">PAS</span>
+                        </span>
+                    </div>
+                    <div class="pl-[2.3rem]">
+                        <span class="flex text-lg uppercase">
+                            <span class="font-bold mr-[0.3rem]">${player.dribbling}</span><span class="font-light">DRI</span>
+                        </span>
+                        <span class="flex text-lg uppercase">
+                            <span class="font-bold mr-[0.3rem]">${player.defending}</span><span class="font-light">DEF</span>
+                        </span>
+                        <span class="flex text-lg uppercase">
+                            <span class="font-bold mr-[0.3rem]">${player.physical}</span><span class="font-light">PHY</span>
+                        </span>
+                    </div>`;
+            }
 
-//             cardHTML += `
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>`;
+            cardHTML += `
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
 
-//             // Append each card to the slider
-//             playerSlider.innerHTML += cardHTML;
-//         });
-//     })
-//     .catch(error => console.error('Error fetching player data:', error));
+            // Append each card to the slider
+            playerSlider.innerHTML += cardHTML;
+        });
+    })
+    .catch(error => console.error('Error fetching player data:', error));
+
